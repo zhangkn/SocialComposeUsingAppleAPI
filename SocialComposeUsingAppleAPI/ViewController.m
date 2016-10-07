3ac4b33203b6f6c2d94fbbfc4df7095d06c14dfc
kevin <929118967@qq.com>
2016-10-07 22:29:34 +0800

    init

--- SocialComposeUsingAppleAPI/ViewController.m
+++ SocialComposeUsingAppleAPI/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
 
 @interface ViewController ()
 
@@ -16,14 +17,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //判断服务是否可用
+    /**
+     SOCIAL_EXTERN NSString *const SLServiceTypeTwitter NS_AVAILABLE(10_8, 6_0);
+     SOCIAL_EXTERN NSString *const SLServiceTypeFacebook NS_AVAILABLE(10_8, 6_0);
+     SOCIAL_EXTERN NSString *const SLServiceTypeSinaWeibo NS_AVAILABLE(10_8, 6_0);
+     SOCIAL_EXTERN NSString *const SLServiceTypeTencentWeibo NS_AVAILABLE(10_9, 7_0);
+     SOCIAL_EXTERN NSString *const SLServiceTypeLinkedIn NS_AVAILABLE(10_9, NA);*/
+    if (![SLComposeViewController isAvailableForServiceType:SLServiceTypeSinaWeibo]) {
+        return;//没有添加Weibo账号，将返回0
+    }
+    //创建socialVC
+    SLComposeViewController *slSocialVC = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+    // 设置一些属性
+    slSocialVC.initialText =@"initialText";
+    [slSocialVC addImage:[UIImage imageNamed:@"connection.jpg"]];
+    //show SocialVC
+    //    slSocialVC
+    [self  presentViewController:slSocialVC animated:YES completion:nil];
+    //监听分享状态
+    slSocialVC.completionHandler = ^(SLComposeViewControllerResult result){
+        switch (result) {
+            case SLComposeViewControllerResultCancelled:
+                NSLog(@"SLComposeViewControllerResultCancelled");
+                break;
+                
+            case SLComposeViewControllerResultDone:
+                NSLog(@"SLComposeViewControllerResultDone");
+                break;
+        }
+    };
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
